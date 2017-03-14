06963efd10fd280821dc1e12b1b039dd8bada3ce
spartakusQ <spartakus1688@gmail.com>
2017-03-14 12:43:56 +0300

    Add Client Migration

--- db/migrate/20170313171557_create_clients.rb
+++ db/migrate/20170313171557_create_clients.rb
@@ -1,4 +1,15 @@
 class CreateClients < ActiveRecord::Migration
   def change
-  end
+    create_table :client do |t|
+
+          t.text :name
+          t.text :phone
+          t.text :datestamp
+          t.text :barber
+          t.text :color
+
+          t.timestamps
+
+      end
+    end
 end
